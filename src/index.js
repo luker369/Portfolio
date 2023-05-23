be32e44d8957fbf770f67faf0e8f5c312a31030a
luker369 <walker0369@gmail.com>
2023-05-23 15:08:26 -0500

    deleted jsbin and millerhome

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import MillerHome from './MillerHome';
+// import MillerHome from './MillerHome';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
